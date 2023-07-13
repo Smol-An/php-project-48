dda6d4dc03dc894b32d802bb9e492ee8b5282cc3
Anastasia Smolyakova <smolyakova_av@mail.ru>
2023-07-13 19:42:50 +0300

    add gendiff

---
+++ src/docopt.php
@@ -0,0 +1,14 @@
+<?php
+
+$doc = <<<DOC
+Generate diff
+
+Usage:
+  gendiff (-h|--help)
+  gendiff (-v|--version)
+
+Options:
+  -h --help                     Show this screen
+  -v --version                  Show version
+
+DOC;
