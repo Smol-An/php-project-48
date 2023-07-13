b6edbbc738494748dbf5e752ff263eb9799cf182
Anastasia Smolyakova <smolyakova_av@mail.ru>
2023-07-13 20:08:05 +0300

    add format

--- src/docopt.php
+++ src/docopt.php
@@ -1,14 +1,15 @@
 <?php
-
 $doc = <<<DOC
 Generate diff
 
 Usage:
   gendiff (-h|--help)
   gendiff (-v|--version)
+  gendiff [--format <fmt>] <firstFile> <secondFile>
 
 Options:
   -h --help                     Show this screen
   -v --version                  Show version
+  --format <fmt>                Report format [default: stylish]
 
 DOC;
