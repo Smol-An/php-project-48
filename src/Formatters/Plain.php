92e221fd4dd13ce5a4ad962925f6d8437d50cb19
Anastasia Smolyakova <smolyakova_av@mail.ru>
2023-09-14 17:53:36 +0300

    fix genPlainDiff function

--- src/Formatters/Plain.php
+++ src/Formatters/Plain.php
@@ -4,33 +4,28 @@ namespace Differ\Formatters\Plain;
 
 function genPlainDiff(array $diff, array $parentKeys = []): array
 {
-    $output = [];
+    return array_merge(
+        ...array_map(function ($key, $node) use ($parentKeys) {
+            $currentKeys = [...$parentKeys, $key];
+            $propertyPath = implode('.', $currentKeys);
 
-    foreach ($diff as $key => $node) {
-        $currentKeys = [...$parentKeys, $key];
-        $propertyPath = implode('.', $currentKeys);
-
-        switch ($node['status']) {
-            case 'added':
-                $formattedValue = formatValue($node['value']);
-                $output[] = "Property '{$propertyPath}' was added with value: {$formattedValue}";
-                break;
-            case 'removed':
-                $formattedValue = formatValue($node['value']);
-                $output[] = "Property '{$propertyPath}' was removed";
-                break;
-            case 'updated':
-                $formattedOldValue = formatValue($node['oldValue']);
-                $formattedNewValue = formatValue($node['newValue']);
-                $output[] = "Property '{$propertyPath}' was updated. From {$formattedOldValue} to {$formattedNewValue}";
-                break;
-            case 'nested':
-                $output = array_merge($output, genPlainDiff($node['children'], $currentKeys));
-                break;
-        }
-    }
-
-    return $output;
+            switch ($node['status']) {
+                case 'added':
+                    $formattedValue = formatValue($node['value']);
+                    return ["Property '{$propertyPath}' was added with value: {$formattedValue}"];
+                case 'removed':
+                    return ["Property '{$propertyPath}' was removed"];
+                case 'updated':
+                    $formattedOldValue = formatValue($node['oldValue']);
+                    $formattedNewValue = formatValue($node['newValue']);
+                    return ["Property '{$propertyPath}' was updated. From {$formattedOldValue} to {$formattedNewValue}"];
+                case 'nested':
+                    return genPlainDiff($node['children'], $currentKeys);
+                default:
+                    return [];
+            }
+        }, array_keys($diff), $diff)
+    );
 }
 
 function formatValue(mixed $value)
