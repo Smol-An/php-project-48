fee0bff2622937bb3a3fb73cea4363d95ab67fa8
Anastasia Smolyakova <smolyakova_av@mail.ru>
2023-09-16 10:52:55 +0300

    fix some functions

--- src/Differ.php
+++ src/Differ.php
@@ -72,7 +72,7 @@ function getFileContents(string $pathToFile)
     return $fileContents;
 }
 
-function getFileFormat(string $pathToFile)
+function getFileFormat($pathToFile)
 {
     return pathinfo($pathToFile, PATHINFO_EXTENSION);
 }
